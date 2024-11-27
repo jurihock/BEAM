8aa3ea387ad2623e75b95a3f9b3f11922ed6fac1
Paul Stier <paul-stier@gmx.de>
2024-11-27 10:26:33 +0100

    Initial commit

---
+++ BEAM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BEAM.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ BEAM/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BEAM.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
