552cf9d8913f6be58412b2d44f1bc1026fddd457
uypko <uypko@student.kit.edu>
2024-12-14 18:35:56 +0100

    Added Titlebar and moved Menu into Titlebar

--- BEAM/ViewModels/MainWindowViewModel.cs
+++ BEAM/ViewModels/MainWindowViewModel.cs
@@ -14,17 +14,17 @@ public partial class MainWindowViewModel : ViewModelBase
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
-   
-    
-   [ObservableProperty] public partial string File { get; set; } = Configuration.StandardEnglish().FileMenu;
+
+    [ObservableProperty] public partial int TitleBarHeight { get; set; } = 38;
+    [ObservableProperty] public partial string File { get; set; } = Configuration.StandardEnglish().FileMenu;
     [ObservableProperty] public partial string Open { get; set; } = Configuration.StandardEnglish().Open;
     [ObservableProperty] public partial string Exit { get; set; } = Configuration.StandardEnglish().Exit;
     [ObservableProperty] public partial string Edit { get; set; } = Configuration.StandardEnglish().Edit;
     [ObservableProperty] public partial string Paste { get; set; } = Configuration.StandardEnglish().Paste;
     [ObservableProperty] public partial string Copy { get; set; } = Configuration.StandardEnglish().Copy;
-    
+
     [ObservableProperty] private string? _fileText;
-    
+
     [RelayCommand]
     public async Task OpenSequence()
     {
@@ -36,7 +36,7 @@ public partial class MainWindowViewModel : ViewModelBase
     {
         Environment.Exit(0);
     }
-    
+
     private async Task<IStorageFile?> OpenFilePickerAsync()
     {
         if (Application.Current?.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop ||
