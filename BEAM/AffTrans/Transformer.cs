521f63847f375441ce8534a08e8a1a19e0f97d72
Moritz <mgeib@protonmail.com>
2024-12-15 13:27:46 +0100

    test: added tests fpr AffineTransformation.cs

--- BEAM/AffTrans/Transformer.cs
+++ BEAM/AffTrans/Transformer.cs
@@ -4,15 +4,15 @@ namespace BEAM.AffTrans;
 
 public class Transformer
 {
-    float[][] _matrix;
+    double[][] _matrix;
     private Point _shift;
-    private float _inverseDeterminant;
+    private double _inverseDeterminant;
     
-    public Transformer(float[][] matrix, Point shift)
+    public Transformer(double[][] matrix, Point shift)
     {
         _matrix = matrix;
         _shift = shift;
-        float det = _matrix[0][0] * _matrix[1][1] - _matrix[0][1] * _matrix[1][0];
+        double det = _matrix[0][0] * _matrix[1][1] - _matrix[0][1] * _matrix[1][0];
         _inverseDeterminant = 1 / det;
     }
 
@@ -20,9 +20,9 @@ public class Transformer
     {
         //reverse the transformation to find the original coordinates of the sequence
         long transformedX = 
-            (long)((_matrix[1][1] * originalX + -_matrix[0][1] * originalY) * _inverseDeterminant + _shift.X);
+            (long)((_matrix[1][1] * originalX + -_matrix[0][1] * originalY) * _inverseDeterminant - _shift.X + 0.5);
         long transformedY = 
-            (long)((-_matrix[1][0] * originalX + _matrix[0][0] * originalY) * _inverseDeterminant + _shift.Y);
+            (long)((-_matrix[1][0] * originalX + _matrix[0][0] * originalY) * _inverseDeterminant - _shift.Y + 0.5);
         return (transformedX, transformedY);
     }
     
@@ -30,14 +30,14 @@ public class Transformer
     {
         //reverse the transformation to find the original coordinates of the sequence
         long transformedX = 
-            (long)(_matrix[1][1] * originalX * _inverseDeterminant + _shift.X);        
+            (long)(_matrix[1][1] * originalX * _inverseDeterminant + _shift.X + 0.5);        
         return transformedX;
     }
     
     public long TransformY(long originalY)
     {
         long transformedY = 
-            (long)(_matrix[0][0] * originalY * _inverseDeterminant + _shift.Y);
+            (long)(_matrix[0][0] * originalY * _inverseDeterminant + _shift.Y + 0.5);
         return transformedY;
     }
     
