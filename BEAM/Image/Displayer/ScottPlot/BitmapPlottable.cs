d5f48754fe161daf02dc741edcf6c1a50b6960d9
Paul Stier <paul-stier@gmx.de>
2025-01-09 12:31:24 +0100

    Scaling image if necessary

--- BEAM/Image/Displayer/ScottPlot/BitmapPlottable.cs
+++ BEAM/Image/Displayer/ScottPlot/BitmapPlottable.cs
@@ -34,27 +34,6 @@ public sealed class BitmapPlottable : IPlottable
             FilterQuality = SKFilterQuality.None // WTF
         };
 
-        /*var visibleWidth = rp.Plot.Grid.XAxis.Max - rp.Plot.Grid.XAxis.Min;
-        var visibleHeight = rp.Plot.Grid.YAxis.Max - rp.Plot.Grid.YAxis.Min;
-        var scaleX = rp.Canvas.DeviceClipBounds.Width / visibleWidth;
-        var scaleY = rp.Canvas.DeviceClipBounds.Height / visibleHeight;
-        var bitmapScaleX = Bitmap.Width / visibleWidth;
-        var bitmapScaleY = Bitmap.Height / visibleHeight;
-
-        var finalScaleX = double.Min(scaleX, bitmapScaleX);
-        var finalScaleY = double.Min(scaleY, bitmapScaleY);
-        Console.WriteLine($"{finalScaleX}x{finalScaleY}");
-
-        var extracted =
-            new SKBitmap(new SKImageInfo((int)visibleWidth, (int)visibleHeight, Bitmap.ColorType, Bitmap.AlphaType));
-        Bitmap.ExtractSubset(extracted, new SKRectI((int)rp.Plot.Grid.XAxis.Min, (int)rp.Plot.Grid.YAxis.Max,
-            (int)rp.Plot.Grid.XAxis.Max, (int)rp.Plot.Grid.YAxis.Min));
-
-        var bt = extracted.Resize(new SKImageInfo((int)(Bitmap.Width * finalScaleX), (int)(Bitmap.Height * finalScaleY)),
-            SKFilterQuality.None);
-        SKRect dest = Axes.GetPixelRect(new CoordinateRect(0, Bitmap.Width, Bitmap.Height, 0)).ToSKRect();
-        rp.Canvas.DrawBitmap(bt, dest, paint);
-        */
         // Get the visible range
         var cropXMin = rp.Plot.Grid.XAxis.Min;
         var cropXMax = rp.Plot.Grid.XAxis.Max;
@@ -80,19 +59,19 @@ public sealed class BitmapPlottable : IPlottable
 
         Bitmap.ExtractSubset(croppedBitmap, cropRect);
 
-// Step 4: Scale the bitmap
-        /*int screenWidth = (int)rp.Canvas.DeviceClipBounds.Width;
-        int screenHeight = (int)rp.Canvas.DeviceClipBounds.Height;
-        float scaleX = (float)screenWidth / (cropXMax - cropXMin);
-        float scaleY = (float)screenHeight / (cropYMax - cropYMin);
+        // Scale the bitmap
+        var screenWidth = rp.Canvas.DeviceClipBounds.Width;
+        var screenHeight = rp.Canvas.DeviceClipBounds.Height;
+        var scaleX = screenWidth / (cropXMax - cropXMin);
+        var scaleY = screenHeight / (cropYMax - cropYMin);
+        scaleX = Math.Min(scaleX, 1);
+        scaleY = Math.Min(scaleY, 1);
 
         using var scaledBitmap = croppedBitmap.Resize(
             new SKImageInfo((int)(croppedBitmap.Width * scaleX), (int)(croppedBitmap.Height * scaleY)),
-            SKFilterQuality.High);*/
-
-// Step 5: Render the bitmap
+            SKFilterQuality.None);
 
         SKRect dest = Axes.GetPixelRect(new CoordinateRect(cropXMin - diffX, cropXMin + croppedBitmap.Width - diffX, cropYMin + croppedBitmap.Height - diffY, cropYMin - diffY)).ToSKRect();
-        rp.Canvas.DrawBitmap(croppedBitmap, dest, paint);
+        rp.Canvas.DrawBitmap(scaledBitmap, dest, paint);
     }
 }
