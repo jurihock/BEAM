1b6d1a9226d8ea011499b398bbb97744a9df19e6
utyxh <utyxh@student.kit.edu>
2025-01-22 21:20:06 +0100

    Created RenderTypes.cs as Enum listing all concrete Subclasses of SequenceRenderer.cs

---
+++ BEAM/Renderer/RenderTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace BEAM.Renderer;
+
+/// <summary>
+/// Enum containing an element for all implemented, concrete subclasses of SequenceRenderer
+/// </summary>
+public enum RenderTypes
+{
+    HeatMapRendererRb,
+    ChannelMapRenderer
+}
