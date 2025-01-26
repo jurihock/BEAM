bb99f7d9a65644393b33269efba2a55fb4c4557a
utyxh <utyxh@student.kit.edu>
2025-01-26 21:40:56 +0100

    Added Interfaces for Analysis of pixels and rectangular regions of pixels

---
+++ BEAM/Analysis/IPixelAnalysis.cs
@@ -0,0 +1,15 @@
+﻿using BEAM.Datatypes;
+using BEAM.ImageSequence;
+using BEAM.ViewModels;
+
+namespace BEAM.Analysis;
+
+/// <summary>
+/// Interface for subclasses to analyse the channels of single pixels.
+/// Implemented as a Strategy design pattern.
+/// </summary>
+public interface IPixelAnalysis
+{
+    public Avalonia.Controls.Control analysePixel(Sequence sequence, Coordinate2D position);
+    public Avalonia.Controls.Control analysePixel(SequenceViewModel viewModel, Coordinate2D position);
+}
---
+++ BEAM/Analysis/IRegionAnalysis.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using BEAM.Datatypes;
+using BEAM.ImageSequence;
+using BEAM.ViewModels;
+
+namespace BEAM.Analysis;
+
+/// <summary>
+/// Interface for subclasses to analyse the channels a rectangular region of pixels.
+/// Implemented as a Strategy design pattern.
+/// </summary>
+public interface IRegionAnalysis
+{
+    public Avalonia.Controls.Control analyseRegion(Sequence sequence, Rectangle region);
+    public Avalonia.Controls.Control analyseRegion(SequenceViewModel viewModel, Rectangle region);
+
+    public Avalonia.Controls.Control analyseRegion(long posX, long posY, long width, long height,
+        SequenceViewModel viewModel);
+}
