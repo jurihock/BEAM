a68520cc7b9e3a72a7ae2d551c6c7e18b2cf378b
SunSon <niklashermerding@protonmail.com>
2025-01-29 20:46:16 +0100

    Added Basic Functionality needed to reroute events to synced Sequences

---
+++ BEAM/ImageSequence/Synchronization/Manipulators/EventSourceMapper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Avalonia.Interactivity;
+using ScottPlot.Avalonia;
+
+namespace BEAM.ImageSequence.Synchronization.Manipulators;
+
+public static class EventSourceMapper
+{
+    private static Queue<RoutedEventArgs> _eventQueue = new();
+    private static Dictionary<RoutedEventArgs, AvaPlot?> _sourcePlots = new();
+
+    public static void AddEventSource(RoutedEventArgs eventSource, AvaPlot? plot)
+    {
+        if (_eventQueue.Count >= 100)
+        {
+            var e = _eventQueue.Dequeue();
+            _sourcePlots.Remove(e);
+        }
+        _eventQueue.Enqueue(eventSource);
+        _sourcePlots.Add(eventSource, plot);
+    }
+    
+    public static bool IsSource(RoutedEventArgs e, AvaPlot plot) {
+        if (_sourcePlots.TryGetValue(e, out var source))
+        {
+            return source != null && source.Equals(plot);
+        }
+        return false;
+    }
+
+    public static void AddIfNotExists(RoutedEventArgs e, AvaPlot plot)
+    {
+        if (!_sourcePlots.ContainsKey(e))
+        {
+            AddEventSource(e, plot);
+        }
+    }
+    
+    public static void Clear()
+    {
+        _eventQueue.Clear();
+        _sourcePlots.Clear();
+    }
+}
--- BEAM/ImageSequence/Synchronization/Manipulators/MouseManipulator.cs
+++ BEAM/ImageSequence/Synchronization/Manipulators/MouseManipulator.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Avalonia.Interactivity;
+using BEAM.Log;
 using ScottPlot.Avalonia;
 
 namespace BEAM.ImageSequence.Synchronization.Manipulators;
@@ -13,142 +15,115 @@ public class MouseManipulator : Manipulator
     public override bool SyncPlot(AvaPlot avaPlot)
     {
         _avaPlots.Add(avaPlot);
-        Console.WriteLine("Added Plot to Manipulator");
         
         avaPlot.PointerEntered += (s, e) =>
         {
-            if (e.Handled)
+            EventSourceMapper.AddIfNotExists(e, avaPlot);
+            if (EventSourceMapper.IsSource(e, avaPlot))
             {
-                return;
-            }
-            e.Handled = true;
-            Console.WriteLine("Fired!");
-            foreach (var plot in _avaPlots.Where(p => p != avaPlot))
-            {
-                plot.RaiseEvent(e);
+                foreach (var plot in _avaPlots.Where(p => p != avaPlot))
+                {
+                    plot.RaiseEvent(e);
+                }
             }
         };
-        
+
         avaPlot.PointerExited += (s, e) =>
         {
-            if (e.Handled)
+            EventSourceMapper.AddIfNotExists(e, avaPlot);
+            if (EventSourceMapper.IsSource(e, avaPlot))
             {
-                return;
-            }
-            e.Handled = true;
-            foreach (var plot in _avaPlots.Where(p => p != avaPlot))
-            {
-                plot.RaiseEvent(e);
+                foreach (var plot in _avaPlots.Where(p => p != avaPlot))
+                {
+                    plot.RaiseEvent(e);
+                }
             }
         };
-        
+
         avaPlot.PointerMoved += (s, e) =>
         {
-            if (e.Handled)
+            EventSourceMapper.AddIfNotExists(e, avaPlot);
+            if (EventSourceMapper.IsSource(e, avaPlot))
             {
-                return;
-            }
-            e.Handled = true;
-            foreach (var plot in _avaPlots.Where(p => p != avaPlot))
-            {
-                plot.RaiseEvent(e);
+                foreach (var plot in _avaPlots.Where(p => p != avaPlot))
+                {
+                    plot.RaiseEvent(e);
+                }
             }
         };
-        
+
         avaPlot.PointerPressed += (s, e) =>
         {
-            if (e.Handled)
+            EventSourceMapper.AddIfNotExists(e, avaPlot);
+            if (EventSourceMapper.IsSource(e, avaPlot))
             {
-                return;
-            }
-            e.Handled = true;
-            foreach (var plot in _avaPlots.Where(p => p != avaPlot))
-            {
-                plot.RaiseEvent(e);
+                foreach (var plot in _avaPlots.Where(p => p != avaPlot))
+                {
+                    plot.RaiseEvent(e);
+                }
             }
         };
-        
+
         avaPlot.PointerReleased += (s, e) =>
         {
-            if (e.Handled)
+            EventSourceMapper.AddIfNotExists(e, avaPlot);
+            if (EventSourceMapper.IsSource(e, avaPlot))
             {
-                return;
-            }
-            e.Handled = true;
-            foreach (var plot in _avaPlots.Where(p => p != avaPlot))
-            {
-                plot.RaiseEvent(e);
+                foreach (var plot in _avaPlots.Where(p => p != avaPlot))
+                {
+                    plot.RaiseEvent(e);
+                }
             }
         };
-        
+
         avaPlot.PointerWheelChanged += (s, e) =>
         {
-            if (e.Handled)
+            EventSourceMapper.AddIfNotExists(e, avaPlot);
+            if (EventSourceMapper.IsSource(e, avaPlot))
             {
-                return;
-            }
-            e.Handled = true;
-            foreach (var plot in _avaPlots.Where(p => p != avaPlot))
-            {
-                plot.RaiseEvent(e);
-            }
-        };
-        
-        avaPlot.PointerCaptureLost += (s, e) =>
-        {
-            if (e.Handled)
-            {
-                return;
-            }
-            e.Handled = true;
-            foreach (var plot in _avaPlots.Where(p => p != avaPlot))
-            {
-                plot.RaiseEvent(e);
+                foreach (var plot in _avaPlots.Where(p => p != avaPlot))
+                {
+                    plot.RaiseEvent(e);
+                }
             }
         };
-        
+
         avaPlot.Tapped += (s, e) =>
         {
-            if (e.Handled)
+            EventSourceMapper.AddIfNotExists(e, avaPlot);
+            if (EventSourceMapper.IsSource(e, avaPlot))
             {
-                return;
-            }
-
-            e.Handled = true;
-            foreach (var plot in _avaPlots.Where(p => p != avaPlot))
-            {
-                plot.RaiseEvent(e);
+                foreach (var plot in _avaPlots.Where(p => p != avaPlot))
+                {
+                    plot.RaiseEvent(e);
+                }
             }
         };
-        
+
         avaPlot.DoubleTapped += (s, e) =>
         {
-            if (e.Handled)
+            EventSourceMapper.AddIfNotExists(e, avaPlot);
+            if (EventSourceMapper.IsSource(e, avaPlot))
             {
-                return;
-            }
-
-            e.Handled = true;
-            foreach (var plot in _avaPlots.Where(p => p != avaPlot))
-            {
-                plot.RaiseEvent(e);
+                foreach (var plot in _avaPlots.Where(p => p != avaPlot))
+                {
+                    plot.RaiseEvent(e);
+                }
             }
         };
-        
+
         avaPlot.Holding += (s, e) =>
         {
-            if (e.Handled)
+            EventSourceMapper.AddIfNotExists(e, avaPlot);
+            if (EventSourceMapper.IsSource(e, avaPlot))
             {
-                return;
-            }
-
-            e.Handled = true;
-            foreach (var plot in _avaPlots.Where(p => p != avaPlot))
-            {
-                plot.RaiseEvent(e);
+                foreach (var plot in _avaPlots.Where(p => p != avaPlot))
+                {
+                    plot.RaiseEvent(e);
+                }
             }
         };
-        
+            
         return true;
     }
 
