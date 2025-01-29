7b0d036c5ee6ae5c1f5b934cb3838e0e48eb1e1f
Moritz <mgeib@protonmail.com>
2025-01-29 17:01:06 +0100

    added scott plot to inspection view

--- BEAM/Views/InspectionView.axaml.cs
+++ BEAM/Views/InspectionView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using ScottPlot.Avalonia;
 
 namespace BEAM.Views;
 
@@ -9,5 +10,14 @@ public partial class InspectionView : UserControl
     public InspectionView()
     {
         InitializeComponent();
+        double[] dataX = { 1, 2, 3, 2.5, 2};
+        double[] dataY = { 1, 4, 9, 16, 25 };
+
+        AvaPlot resultPlot = this.Find<AvaPlot>("AvaPlot2");
+        resultPlot.Plot.Add.Bars(dataX);
+        resultPlot.Plot.Axes.Margins(bottom: 0);
+        resultPlot.Refresh();
     }
+    
+    
 }
