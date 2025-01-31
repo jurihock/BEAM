6ff5999a9a3d04b57ed473d385398ec36e60c107
utyxh <utyxh@student.kit.edu>
2025-01-31 15:54:22 +0100

    Updated Return value to AbstractAnalysisView

--- BEAM/Analysis/PixelAnalysisChannel.cs
+++ BEAM/Analysis/PixelAnalysisChannel.cs
@@ -2,24 +2,25 @@
 using BEAM.Datatypes;
 using BEAM.ImageSequence;
 using BEAM.ViewModels;
+using BEAM.Views.AnalysisView;
 
 namespace BEAM.Analysis;
 
 public class PixelAnalysisChannel : IPixelAnalysis
     
 {
-    public Control analysePixel(Sequence sequence, Coordinate2D position)
+    public AbstractAnalysisView analysePixel(Sequence sequence, Coordinate2D position)
     {
         double[] colors = sequence.GetPixel((long) position.Column, (long) position.Row);
         return null;
     }
 
-    public Control analysePixel(SequenceViewModel viewModel, Coordinate2D position)
+    public AbstractAnalysisView analysePixel(SequenceViewModel viewModel, Coordinate2D position)
     {
         throw new System.NotImplementedException();
     }
 
-    private Control _CreateGraph(double[] colors)
+    private AbstractAnalysisView _CreateGraph(double[] colors)
     {
         var graph = new Control();
         return null;
