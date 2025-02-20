e577d1e541f2c272977420907af3db62cafa42b5
David Frank Siekacz <57222895+ChaosDavos@users.noreply.github.com>
2025-02-20 22:45:55 +0100

    Added some Minimap Unit Tests

---
+++ BEAM.Tests/Minimap/MinimapSettingsProviderTest.cs
@@ -0,0 +1,32 @@
+﻿using BEAM.Image.Minimap.Utility;
+
+namespace BEAM.Tests.Minimap;
+
+public class MinimapSettingsProviderTests
+{
+    [Fact]
+    public void Constructor_ShouldInitializeDefaultMinimaps()
+    {
+        var provider = new MinimapSettingsProvider();
+        Assert.NotEmpty(provider.GetDefaultObjects());
+    }
+
+    [Fact]
+    public void GetDefaultObject_ShouldReturnCurrentDefault()
+    {
+        var provider = new MinimapSettingsProvider();
+        var defaultObject = provider.GetDefaultObject();
+
+        Assert.NotNull(defaultObject);
+    }
+
+    [Fact]
+    public void SetDefaultObject_ShouldUpdateCurrentDefault()
+    {
+        var provider = new MinimapSettingsProvider();
+        var newDefault = provider.GetDefaultObjects()[0];
+        provider.SetDefaultObject(newDefault);
+
+        Assert.Equal(newDefault, provider.GetDefaultObject());
+    }
+}
