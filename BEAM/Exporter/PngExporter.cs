8a1a3f8a597b1329fcb6c66694d6a86897629175
Niklas Hermerding <niklashermerding@gmail.com>
2025-02-21 16:29:53 +0100

    Fixed PngExporter.cs not exporting the last part, when the sequence height is not a multiple of MaxHeight = 4096

--- BEAM/Exporter/PngExporter.cs
+++ BEAM/Exporter/PngExporter.cs
@@ -25,17 +25,39 @@ public static class PngExporter
     /// <param name="renderer">The renderer used for the sequence.</param>
     public static void Export(IStorageFile path, TransformedSequence sequence, SequenceRenderer renderer)
     {
+        var i = 0;
         Directory.CreateDirectory(path.Path.AbsolutePath);
         var shape = sequence.Shape;
-        for (var i = 0; i <= shape.Height / MaxHeight; i++)
+        for (; i < shape.Height / MaxHeight; i++)
         {
-            var height = i - 1 == shape.Height / MaxHeight ? shape.Height % MaxHeight : MaxHeight;
-            var bitmap = new SKBitmap((int) shape.Width, (int) height, SKColorType.Bgra8888, SKAlphaType.Opaque);
+            var bitmap = new SKBitmap((int) shape.Width, (int) MaxHeight, SKColorType.Bgra8888, SKAlphaType.Opaque);
             for (var j = 0; j < MaxHeight && j + i * MaxHeight < shape.Height; j++)
             {
                 for (var k = 0; k < shape.Width; k++)
                 {
                     var data = renderer.RenderPixel(sequence, k, j + i * MaxHeight);
+                    bitmap.SetPixel(k, j, new SKColor(data.R, data.G, data.B, 255));
+                }
+            }
+
+            using var image = SKImage.FromBitmap(bitmap);
+            using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
+            {
+                using (var stream = File.OpenWrite(Path.Combine(path.Path.AbsolutePath, $"{i:D8}.png")))
+                {
+                    data.SaveTo(stream);
+                }
+            }
+        }
+        if (shape.Height % MaxHeight != 0)
+        {
+            var height = shape.Height % MaxHeight;
+            var bitmap = new SKBitmap((int) shape.Width, (int) height, SKColorType.Bgra8888, SKAlphaType.Opaque);
+            for (var j = 0; j < height; j++)
+            {
+                for (var k = 0; k < shape.Width; k++)
+                {
+                    var data = renderer.RenderPixel(sequence, k, j + shape.Height - height);
                     bitmap.SetPixel(k, j, new SKColor(data.R, data.G, data.B, 255));
                 }
             }
