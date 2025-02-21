1cf4acd87a30f42f3848175ff670ae38eb6bae0a
uypko <uypko@student.kit.edu>
2025-02-21 18:16:04 +0100

    Changed Right CLick Menu "Inspect Pixel" to "Inspect"

--- BEAM/Views/SequenceView.axaml.cs
+++ BEAM/Views/SequenceView.axaml.cs
@@ -218,7 +218,7 @@ public partial class SequenceView : UserControl
         var menu = AvaPlot1.Menu!;
         menu.Clear();
         
-        menu.Add("Inspect Pixel",
+        menu.Add("Inspect",
             _ => _OpenInspectionViewModel());
         menu.AddSeparator();
         menu.Add("Sync to this",
