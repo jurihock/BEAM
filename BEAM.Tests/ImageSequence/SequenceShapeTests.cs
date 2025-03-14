62c4850103f983b43ef9a38ce3bdec4d236c4934
Niklas Hermerding <niklashermerding@gmail.com>
2025-03-14 16:22:47 +0100

    Added Tests for SequenceShape

---
+++ BEAM.Tests/ImageSequence/SequenceShapeTests.cs
@@ -0,0 +1,95 @@
+using BEAM.ImageSequence;
+
+namespace BEAM.Tests.ImageSequence;
+using Xunit;
+
+public class SequenceShapeTests
+{
+    [Fact]
+    public void Equals_ReturnsTrue_ForIdenticalShapes()
+    {
+        var shape1 = new SequenceShape(1920, 1080, 3);
+        var shape2 = new SequenceShape(1920, 1080, 3);
+
+        Assert.True(shape1.Equals(shape2));
+    }
+
+    [Fact]
+    public void Equals_ReturnsFalse_ForDifferentShapes()
+    {
+        var shape1 = new SequenceShape(1920, 1080, 3);
+        var shape2 = new SequenceShape(1280, 720, 3);
+
+        Assert.False(shape1.Equals(shape2));
+    }
+
+    [Fact]
+    public void Equals_ReturnsFalse_WhenOtherIsNull()
+    {
+        var shape = new SequenceShape(1920, 1080, 3);
+
+        Assert.False(shape.Equals(null));
+    }
+
+    [Fact]
+    public void GetHashCode_ReturnsSameHashCode_ForIdenticalShapes()
+    {
+        var shape1 = new SequenceShape(1920, 1080, 3);
+        var shape2 = new SequenceShape(1920, 1080, 3);
+
+        Assert.Equal(shape1.GetHashCode(), shape2.GetHashCode());
+    }
+
+    [Fact]
+    public void GetHashCode_ReturnsDifferentHashCode_ForDifferentShapes()
+    {
+        var shape1 = new SequenceShape(1920, 1080, 3);
+        var shape2 = new SequenceShape(1280, 720, 3);
+
+        Assert.NotEqual(shape1.GetHashCode(), shape2.GetHashCode());
+    }
+
+    [Fact]
+    public void ToString_ReturnsCorrectFormat()
+    {
+        var shape = new SequenceShape(1920, 1080, 3);
+
+        Assert.Equal("1920x1080x3", shape.ToString());
+    }
+
+    [Fact]
+    public void OperatorEquals_ReturnsTrue_ForIdenticalShapes()
+    {
+        var shape1 = new SequenceShape(1920, 1080, 3);
+        var shape2 = new SequenceShape(1920, 1080, 3);
+
+        Assert.True(shape1 == shape2);
+    }
+
+    [Fact]
+    public void OperatorEquals_ReturnsFalse_ForDifferentShapes()
+    {
+        var shape1 = new SequenceShape(1920, 1080, 3);
+        var shape2 = new SequenceShape(1280, 720, 3);
+
+        Assert.False(shape1 == shape2);
+    }
+
+    [Fact]
+    public void OperatorNotEquals_ReturnsTrue_ForDifferentShapes()
+    {
+        var shape1 = new SequenceShape(1920, 1080, 3);
+        var shape2 = new SequenceShape(1280, 720, 3);
+
+        Assert.True(shape1 != shape2);
+    }
+
+    [Fact]
+    public void OperatorNotEquals_ReturnsFalse_ForIdenticalShapes()
+    {
+        var shape1 = new SequenceShape(1920, 1080, 3);
+        var shape2 = new SequenceShape(1920, 1080, 3);
+
+        Assert.False(shape1 != shape2);
+    }
+}
