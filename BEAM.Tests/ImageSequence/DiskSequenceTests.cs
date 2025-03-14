128cc64fff046d82ec59353c495c8afe2beb2a71
utyxh <utyxh@student.kit.edu>
2025-03-14 10:45:32 +0100

    Added Test to load simple Envi sequence

--- BEAM.Tests/ImageSequence/DiskSequenceTests.cs
+++ BEAM.Tests/ImageSequence/DiskSequenceTests.cs
@@ -72,9 +72,9 @@ public class DiskSequenceTests
     /// <summary>
     /// Tests opening a PNG sequence consisting of 3 images (each 10x10x4)
     /// </summary>
-    /// <exception cref="Exception"></exception>
+    /// <exception cref="Exception">If opening the file fails, an exception is thrown</exception>
     [Fact]
-    public void TestOpeningRgbwSequenceRgb3Images()
+    public void TestOpeningRgbwSequenceRgb3ImagesPNG()
     {
         var testSequenceFolder = "";
         try
@@ -88,7 +88,7 @@ public class DiskSequenceTests
 
             
             // Use a well-defined path test directory
-            testSequenceFolder = Path.Combine(baseDirectory, "TestSequence", "TestSequenceRGB");
+            testSequenceFolder = Path.Combine(baseDirectory, "TestSequence", "TestSequenceRGBPNG");
 
             // Check if the directory exists
             if (!Directory.Exists(testSequenceFolder))
@@ -108,6 +108,66 @@ public class DiskSequenceTests
         Assert.NotNull(diskSequence);
 
         Assert.Equal(diskSequence.GetLoadedImageCount(), 3);
+        // test correct shape of the Sequence
+        var shape = new ImageShape(10, 30, 4);
+        Assert.Equal(shape, diskSequence.Shape);
+        
+        // test correct pixel values read (BGR)
+        // test access to first image
+        Assert.Equal([0, 0, 255, 255], diskSequence.GetPixel(0,0));
+        Assert.Equal([0, 0, 255, 255], diskSequence.GetPixel(9,9));
+
+        // test access to second image
+        Assert.Equal([0, 255, 0, 255], diskSequence.GetPixel(0, 10));
+        Assert.Equal([0, 255, 0, 255], diskSequence.GetPixel(9,19));
+        
+        // test access to third image
+        Assert.Equal([255, 0, 0, 255], diskSequence.GetPixel(0,20));
+        Assert.Equal([255, 0, 0, 255], diskSequence.GetPixel(9,29));
+        //Assert.Throws<>()
+        
+        diskSequence.Dispose();
+    }
+    
+    /// <summary>
+    /// Tests opening an ENVI sequence version of the sequence tested above
+    /// </summary>
+    /// <exception cref="Exception"></exception>
+    [Fact]
+    public void TestOpeningRgbwSequenceRgb3ImagesEnvi()
+    {
+        var testSequenceFolder = "";
+        try
+        {
+            // Create the base directory dynamically
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            baseDirectory = Directory.GetParent(baseDirectory)!.FullName;
+            baseDirectory = Directory.GetParent(baseDirectory)!.FullName;
+            baseDirectory = Directory.GetParent(baseDirectory)!.FullName;
+            baseDirectory = Directory.GetParent(baseDirectory)!.FullName;
+
+            
+            // Use a well-defined path test directory
+            testSequenceFolder = Path.Combine(baseDirectory, "TestSequence", "TestSequenceRGBENVI");
+
+            // Check if the directory exists
+            if (!Directory.Exists(testSequenceFolder))
+            {
+                throw new Exception($"Cannot find the folder at: {testSequenceFolder}");
+            }
+
+            _testOutputHelper.WriteLine("Folder found successfully!");
+        }
+        catch (Exception ex)
+        {
+            _testOutputHelper.WriteLine($"Error: {ex.Message}");
+        }
+        
+        // open the test Sequence
+        var diskSequence = DiskSequence.Open(testSequenceFolder);
+        Assert.NotNull(diskSequence);
+
+        Assert.Equal(diskSequence.GetLoadedImageCount(), 2);
         // test correct shape of the Sequence
         var shape = new ImageShape(10, 30, 4);
         Assert.Equal(shape, diskSequence.Shape);
