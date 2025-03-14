1d897cbbcb233d93572a4223a9b65bf209f246c1
utyxh <utyxh@student.kit.edu>
2025-03-14 10:32:09 +0100

    Added tests for DiscSequence

--- BEAM.Tests/ImageSequence/DiskSequenceTests.cs
+++ BEAM.Tests/ImageSequence/DiskSequenceTests.cs
@@ -1,8 +1,12 @@
-﻿using BEAM.ImageSequence;
+﻿using BEAM.Image;
+using BEAM.ImageSequence;
 using Xunit.Abstractions;
 
 namespace BEAM.Tests.ImageSequence;
 
+/// <summary>
+/// Test for the class 
+/// </summary>
 public class DiskSequenceTests
 {
     private readonly ITestOutputHelper _testOutputHelper;
@@ -12,18 +16,22 @@ public class DiskSequenceTests
         _testOutputHelper = testOutputHelper;
     }
 
+    /// <summary>
+    /// Tests opening a PNG sequence with only 4 pixels
+    /// </summary>
+    /// <exception cref="Exception"></exception>
     [Fact]
-    public void TestOpeningRgbwSequence()
+    public void TestOpeningRgbw4Sequence_OpenAsFile()
     {
-        string testSequenceFolder = "";
+        var testSequenceFolder = "";
         try
         {
             // Create the base directory dynamically
             string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            baseDirectory = Directory.GetParent(baseDirectory).FullName;
-            baseDirectory = Directory.GetParent(baseDirectory).FullName;
-            baseDirectory = Directory.GetParent(baseDirectory).FullName;
-            baseDirectory = Directory.GetParent(baseDirectory).FullName;
+            baseDirectory = Directory.GetParent(baseDirectory)!.FullName;
+            baseDirectory = Directory.GetParent(baseDirectory)!.FullName;
+            baseDirectory = Directory.GetParent(baseDirectory)!.FullName;
+            baseDirectory = Directory.GetParent(baseDirectory)!.FullName;
 
             
             // Use a well-defined path test directory
@@ -42,11 +50,81 @@ public class DiskSequenceTests
             _testOutputHelper.WriteLine($"Error: {ex.Message}");
         }
         
+        string testSequenceFilePath = Path.Combine(testSequenceFolder, "RGBW_VierPixelSequenz.png");
+        List<string> filepaths = [testSequenceFilePath];
+        // open the test Sequence
+        var diskSequence = DiskSequence.Open(filepaths, "Test");
+        Assert.NotNull(diskSequence);
+        // test correct pixel values read (BGR)
+        Assert.Equal([0, 0, 255, 255], diskSequence.GetPixel(0,0));
+        Assert.Equal([0, 255, 0, 255], diskSequence.GetPixel(1,0));
+        Assert.Equal([255, 0, 0, 255], diskSequence.GetPixel(0,1));
+        Assert.Equal([255, 255, 255, 255], diskSequence.GetPixel(1,1));
+        //Assert.Throws<>()
+        
+        // test correct shape of the Image
+        var shape = new ImageShape(2, 2, 4);
+        Assert.Equal(shape, diskSequence.Shape);
+        
+        diskSequence.Dispose();
+    }
+    
+    /// <summary>
+    /// Tests opening a PNG sequence consisting of 3 images (each 10x10x4)
+    /// </summary>
+    /// <exception cref="Exception"></exception>
+    [Fact]
+    public void TestOpeningRgbwSequenceRgb3Images()
+    {
+        var testSequenceFolder = "";
+        try
+        {
+            // Create the base directory dynamically
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            baseDirectory = Directory.GetParent(baseDirectory)!.FullName;
+            baseDirectory = Directory.GetParent(baseDirectory)!.FullName;
+            baseDirectory = Directory.GetParent(baseDirectory)!.FullName;
+            baseDirectory = Directory.GetParent(baseDirectory)!.FullName;
+
+            
+            // Use a well-defined path test directory
+            testSequenceFolder = Path.Combine(baseDirectory, "TestSequence", "TestSequenceRGB");
+
+            // Check if the directory exists
+            if (!Directory.Exists(testSequenceFolder))
+            {
+                throw new Exception($"Cannot find the folder at: {testSequenceFolder}");
+            }
+
+            _testOutputHelper.WriteLine("Folder found successfully!");
+        }
+        catch (Exception ex)
+        {
+            _testOutputHelper.WriteLine($"Error: {ex.Message}");
+        }
+        
         // open the test Sequence
         var diskSequence = DiskSequence.Open(testSequenceFolder);
         Assert.NotNull(diskSequence);
+
+        Assert.Equal(diskSequence.GetLoadedImageCount(), 3);
+        // test correct shape of the Sequence
+        var shape = new ImageShape(10, 30, 4);
+        Assert.Equal(shape, diskSequence.Shape);
         
-        Assert.Equal([255, 0, 0, 255], diskSequence.GetPixel(0,0));
+        // test correct pixel values read (BGR)
+        // test access to first image
+        Assert.Equal([0, 0, 255, 255], diskSequence.GetPixel(0,0));
+        Assert.Equal([0, 0, 255, 255], diskSequence.GetPixel(9,9));
+
+        // test access to second image
+        Assert.Equal([0, 255, 0, 255], diskSequence.GetPixel(0, 10));
+        Assert.Equal([0, 255, 0, 255], diskSequence.GetPixel(9,19));
+        
+        // test access to third image
+        Assert.Equal([255, 0, 0, 255], diskSequence.GetPixel(0,20));
+        Assert.Equal([255, 0, 0, 255], diskSequence.GetPixel(9,29));
+        //Assert.Throws<>()
         
         diskSequence.Dispose();
     }
